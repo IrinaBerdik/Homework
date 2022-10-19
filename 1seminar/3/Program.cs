a5371846aa86d64c792bb356b4b7976ede09c862
IrinaB <i-lezhneva@mail.ru>
2022-10-20 00:17:00 +0300

    добавлено дз по 1 семинару 13.10.22

---
+++ 1seminar/3/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+for (int a=2; a<=N; a+=2)
+   {
+     Console.Write(a);
+     Console.Write(" ");
+    }
