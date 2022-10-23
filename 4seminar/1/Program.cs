f048a55556303c3ae30f8498dfa22aded83c6801
IrinaB <i-lezhneva@mail.ru>
2022-10-24 01:17:25 +0300

    внесены изменения в 1 задачу 4 семинара

--- 4seminar/1/Program.cs
+++ 4seminar/1/Program.cs
@@ -4,6 +4,19 @@
 2, 4 -> 16*/
 Console.WriteLine("Введите два числа");
 int a = int.Parse(Console.ReadLine());
-int n = int.Parse(Console.ReadLine());
-if (n>0) Console.Write(Math.Pow(a,n));
-else Console.Write(Math.Pow(a,-n));
+int b = int.Parse(Console.ReadLine());
+/*if (n>0) Console.Write(Math.Pow(a,n));
+else Console.Write(Math.Pow(a,-n));*/
+void AvStepeniB (int a, int b)
+{   int stepen = 1;
+    int c;
+    if (b<0) c=-b;
+    else c = b;
+        for (int n=1; n <= c; n++)
+        {
+        stepen = stepen * a;
+        }
+Console.Write("А в натуральной степени Б="+stepen);
+}
+AvStepeniB (a, b);
+
