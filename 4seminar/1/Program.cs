3793edf440c99ca843693047e8d454b31075facb
IrinaB <i-lezhneva@mail.ru>
2022-10-23 19:50:18 +0300

    сохранены задачи семинара 4

---
+++ 4seminar/1/Program.cs
@@ -0,0 +1,9 @@
+﻿/*  Напишите цикл, который принимает на вход
+ два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+Console.WriteLine("Введите два числа");
+int a = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+if (n>0) Console.Write(Math.Pow(a,n));
+else Console.Write(Math.Pow(a,-n));
---
+++ 4seminar/3/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите программу, которая задаёт массив
+ из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+
+void FillArray(int[] numbers)
+{
+    int length = numbers.Length;
+    int index=0;
+    while (index<length)
+    {
+        numbers[index]=new Random().Next(1,100);
+        index++;
+    }
+}
+void PrintArray(int[] numbers)
+{
+    int count=numbers.Length;
+    int position =0;
+    while (position<count)
+    {
+        Console.Write(numbers[position] + " ");
+        position++;
+    }
+}
+int[] array=new int[8];
+FillArray(array);
+PrintArray(array);
+
