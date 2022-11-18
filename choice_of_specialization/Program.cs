ba19eb3df9164e23ffb725e05cf72b65ce31686c
IrinaB <i-lezhneva@mail.ru>
2022-11-19 02:24:13 +0300

    commit3 save choice_of_specialization

---
+++ choice_of_specialization/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] array_A = {"to", "7589", "fly", "in", "the", "sky", "hello", ":)"};
+string[] new_array = new string [array_A.Length];
+int j = 0;
+for (int i = 0; i < array_A.Length; i++)
+{
+    if (array_A[i].Length <= 3)
+    {
+        new_array[j] = array_A[i];
+        j++;
+    }
+}
+for (int i = 0; i <= j; i++)
+{
+    Console.Write(new_array[i] + " ");
+}
