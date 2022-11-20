5d3571c99245b8a66f9b71234af4892e9e7333b0
IrinaB <i-lezhneva@mail.ru>
2022-11-20 04:32:10 +0300

    save hw151122

--- Python/hw151122/rle.py
+++ Python/hw151122/rle.py
@@ -1,22 +1,50 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. Пример:
+# На сжатие входные данные: WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW
+# Выходные данные: 12W1B12W3B24W1B14W
+
+def coding(txt):
+    count = 1
+    res = ''
+    for i in range(len(txt)-1):
+        if txt[i] == txt[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt)-2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+    return res
+
+def decoding(txt):
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
+
+
+s = input("Введите текст для кодировки: ")
+print(f"Текст после кодировки: {coding(s)}")
+print(f"Текст после дешифровки: {decoding(coding(s))}")
+
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
-
-
-string = list(input())
-coded_string = ''
-cumul=0
-for i in range(len(string)):
-    i+=cumul
-    base=string[i]
-    k=1
-    while base==string[i+k]: 
-        k+=1 
-        if i+k>len(string):
-            break
-        if k>1:
-            coded_string+=f'{base} {k}'
-            cumul+=k-1
-        
-
-
-
-    
+# с семинара:
+# string = list(input())
+# coded_string = ''
+# cumul=0
+# for i in range(len(string)):
+#     i+=cumul
+#     base=string[i]
+#     k=1
+#     while base==string[i+k]: 
+#         k+=1 
+#         if i+k>len(string):
+#             break
+#         if k>1:
+#             coded_string+=f'{base} {k}'
+#             cumul+=k-1
+            
